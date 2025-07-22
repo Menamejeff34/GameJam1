81540a4579b2ba448ce427d8f4d934cdda88c835
LJ2741 <LJvivas@icloud.com>
2025-07-22 00:02:26 -0400

    Created the enemy template

---
+++ nsjs/Scenes/Enemies/enemy_1_test.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_player: RayCast2D = $RayCastPlayer
+
+const SPEED := 300.0
+var direction := 1
+var player_found := false
+func _physics_process(delta: float) -> void:
+	if player_found:
+		chase()
+	else:
+		patrol()
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+		
+	move_and_slide()
+
+
+
+func patrol():
+	if ray_cast_right.is_colliding():
+		direction = -1
+		ray_cast_player.rotate(rad_to_deg(180))
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		ray_cast_player.rotate(rad_to_deg(180))
+	
+	if ray_cast_player.is_colliding():
+		player_found = true
+		
+		
+	velocity.x = direction * SPEED
+	
+func chase():
+	pass
+	#the plan for this function is to uses an autoload to find the players position
+	#and have the enemy run at them
+	

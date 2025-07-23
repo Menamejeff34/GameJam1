22bcdc912024a52b7e36105720a4d18f423ebf2e
LJ2741 <LJvivas@icloud.com>
2025-07-23 17:51:54 -0400

    Added a flashlight that follows the mouse, player now also follows the mouse

--- nsjs/Scenes/Player/player.gd
+++ nsjs/Scenes/Player/player.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
+@onready var flash_sprite: Sprite2D = $FlashSprite
+@onready var sprite: AnimatedSprite2D = $Sprite
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var direction := 0
 func _ready() -> void:
 	Utility.player_pos = position
 
 func _physics_process(delta: float) -> void:
-	
+	spriteDirection()
+	flashlight()
 	# Needed to allow other nodes to know the player's position
 	Utility.player_pos = position 
 	
@@ -18,10 +22,28 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	
 	move_and_slide()
+
+func spriteDirection():
+	if get_local_mouse_position().x > sprite.position.x:
+		sprite.flip_h = false
+	elif get_local_mouse_position().x < sprite.position.x:
+		sprite.flip_h = true
+
+func flashlight():
+	flash_sprite.look_at(get_global_mouse_position())
+	if sprite.flip_h == false:
+		flash_sprite.position.x = sprite.position.x + 65
+		flash_sprite.rotation_degrees = clamp(flash_sprite.rotation_degrees,-40,40)
+	elif sprite.flip_h == true:
+		flash_sprite.position.x = sprite.position.x - 65
+		flash_sprite.rotation_degrees = clamp(flash_sprite.rotation_degrees,140,230)
+		
+	
